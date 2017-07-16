03bdf71cec499c7ad3dc9503b6c09c95b4727aa6
Olakunle Boye <kunlexzy@gmail.com>
2017-07-16 04:56:13 +0100

    Page Updated

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -12,12 +12,18 @@
    	<div class="row">
    		<div class="col-md-8 col-md-offset-2">
    			<div class="panel panel-default">
-   			<div class="panel-body"> 
+   			<div id="health" class="panel-body"> 
    			{!! $key->content !!}
    		</div>
    		</div>
    		</div>
    	</div>
    </div>
+   <script src="{{asset('js/jquery.min.js')}}"></script>
+   <script type="text/javascript">
+     $(function(){
+    $('#health img').addClass('img img-responsive');
+});
+   </script>
 </body>
 </html>
