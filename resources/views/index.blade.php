89b85d298d4803af62035853f9801ff53eddfcd5
Olakunle Boye <kunlexzy@gmail.com>
2017-10-05 21:43:54 +0100

    Index Updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/froala_editor.pkgd.min.css')}}">
+       <link rel="stylesheet" type="text/css" href="{{asset('/css/froala_style.min.css')}}">
 	<title> {{ $key->title }} </title>
 </head>
 <body>
